17e223ead641aec9fd1ec469c1d342fbdebbe175
Ruiqi Yu <37889326+ruiqiy4-1663775@users.noreply.github.com>
2024-07-01 14:27:36 -0700

    ok

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: 'http://event-search.uniytech.com'  // your production backend URL
+    backendUrl: 'https://event-search.uniytech.com'  // your production backend URL
   };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:8080'  // your development backend URL
+     backendUrl: 'https://event-search.uniytech.com',
+    // backendUrl: 'http://localhost:8080'  
   };
